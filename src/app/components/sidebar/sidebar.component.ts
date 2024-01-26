ef3c571f0af70fe645939a45cac7c7e5fdbddad4
David de Andrés <davdeafon@alu.edu.gva.es>
2024-01-26 09:48:00 +0100

    dietas

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,13 +8,13 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+    { path: '/dashboard', title: 'Inicio',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/icons', title: 'Alimentación',  icon:'ni-planet text-blue', class: '' },
+    { path: '/maps', title: 'Mapa',  icon:'ni-pin-3 text-orange', class: '' },
+    { path: '/tables', title: 'Tienda online (Tables)',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/user-profile', title: 'Perfil de usuario',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/login', title: 'Accede a tu perfil',  icon:'ni-key-25 text-info', class: '' },
+    // { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
 ];
 
 @Component({
