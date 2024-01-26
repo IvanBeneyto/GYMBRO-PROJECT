c5dd37fb6f4e61351567620d40a64ece73ff6535
Ivan Beneyto <granivan98@gmail.com>
2024-01-26 10:38:14 +0100

    Rutas traducidas

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,9 +7,9 @@ import { ClipboardModule } from 'ngx-clipboard';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
+import { AlimentacionComponent } from '../../pages/alimentacion/alimentacion.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { PerfilUsuarioComponent } from '../../pages/perfil-usuario/perfil-usuario.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { ToastrModule } from 'ngx-toastr';
@@ -25,9 +25,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   declarations: [
     DashboardComponent,
-    UserProfileComponent,
+    PerfilUsuarioComponent,
     TablesComponent,
-    IconsComponent,
+    AlimentacionComponent,
     MapsComponent
   ]
 })
